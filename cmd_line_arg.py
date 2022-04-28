2c544b0294be9ec20867f4ff81569b214fd27f15
lawilding <101914639+lawilding@users.noreply.github.com>
2022-04-28 17:38:44 -0600

    Create cmd_line_arg.py

---
+++ cmd_line_arg.py
@@ -0,0 +1,10 @@
+import sys
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("--a")
+
+arg = input("Please input what you would like to search for: ")
+ 
+print("Searching for ", sys.argv[1])
+
